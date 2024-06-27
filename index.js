af9bfa1b3d014a6e2d6f579500703b490224150b
Rahel Gunaratne <rahel.gunaratne@gmail.com>
2024-06-27 01:12:49 -0400

    latest

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 document.getElementById('contactForm').addEventListener('submit', async function(event) {
     event.preventDefault();
 
+    const container = document.getElementById('parent-container');
     const form = document.getElementById('contactForm');
     const loading = document.getElementById('loading');
 
     loading.style.display = 'block';
-    form.classList.add('blur-effect');
+    container.classList.add('blur-effect');
 
     const email = document.getElementById('email').value;
     const idea = document.getElementById('idea').value;
@@ -33,6 +34,7 @@ document.getElementById('contactForm').addEventListener('submit', async function
     } finally {
         // Hide loading indicator and remove blur effect
         loading.style.display = 'none';
-        form.classList.remove('blur-effect');
+        container.classList.remove('blur-effect');
+        form.reset();
     }
 });
